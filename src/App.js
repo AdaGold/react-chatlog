f9dac38c1e710cb8928ad579d1523950e6f9acdd
CheezItMan <chris@adadevelopersacademy.org>
2019-06-11 11:17:32 -0700

    Move App to functional component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 
-class App extends Component {
-  render() {
-    console.log(chatMessages);
+const App = () => {
+  console.log(chatMessages);
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Application title</h1>
-        </header>
-        <main className="App-main">
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Application title</h1>
+      </header>
+      <main className="App-main">
 
-        </main>
-      </div>
-    );
-  }
-}
+      </main>
+    </div>
+  );
+};
 
 export default App;
