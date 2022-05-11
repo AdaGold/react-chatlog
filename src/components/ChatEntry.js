abeec740f4b69e5dee6b86b9a02c29dfd6e8662b
Becca Elenzil <becca@adadevelopersacademy.org>
2022-05-11 08:49:15 -0700

    add like button to ChatEntry scaffold

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -12,6 +12,11 @@ const ChatEntry = (props) => {
       <section className="entry-bubble">
         <p>Replace with body of ChatEntry</p>
         <p className="entry-time">Replace with TimeStamp component</p>
+        <button>
+          <span role="im" aria-label="filled red and empty heart">
+            ❤️🤍
+          </span>
+        </button>
       </section>
     </div>
   );
