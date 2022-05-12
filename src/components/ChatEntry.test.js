a17266442a46d2749b998f1f1a64483557ada1cb
Becca <becca@adadevelopersacademy.org>
2022-05-12 10:57:05 -0700

    update wave03 chatEntry test

--- src/components/ChatEntry.test.js
+++ src/components/ChatEntry.test.js
@@ -1,9 +1,9 @@
-import React from "react";
-import "@testing-library/jest-dom/extend-expect";
-import ChatEntry from "./ChatEntry";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import ChatEntry from './ChatEntry'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 
-describe("Wave 01: ChatEntry", () => {
+describe('Wave 01: ChatEntry', () => {
   beforeEach(() => {
     render(
       <ChatEntry
@@ -11,25 +11,25 @@ describe("Wave 01: ChatEntry", () => {
         body="Get out by 8am.  I'll count the silverware"
         timeStamp="2018-05-18T22:12:03Z"
       />
-    );
-  });
+    )
+  })
 
-  test("renders without crashing and shows the sender", () => {
-    expect(screen.getByText(/Joe Biden/)).toBeInTheDocument();
-  });
+  test('renders without crashing and shows the sender', () => {
+    expect(screen.getByText(/Joe Biden/)).toBeInTheDocument()
+  })
 
-  test("that it will display the body", () => {
-    expect(screen.getByText(/Get out by 8am/)).toBeInTheDocument();
-  });
+  test('that it will display the body', () => {
+    expect(screen.getByText(/Get out by 8am/)).toBeInTheDocument()
+  })
 
-  test("that it will display the time", () => {
-    expect(screen.getByText(/\d+ years ago/)).toBeInTheDocument();
-  });
-});
+  test('that it will display the time', () => {
+    expect(screen.getByText(/\d+ years ago/)).toBeInTheDocument()
+  })
+})
 
-describe("Wave 03: ChatEntry Like Callback", () => {
-  test("when clicked on it calls the callback function", async () => {
-    const callback = jest.fn();
+describe('Wave 03: ChatEntry Like Callback', () => {
+  test('when clicked on it calls the callback function', async () => {
+    const callback = jest.fn()
 
     render(
       <ChatEntry
@@ -38,16 +38,16 @@ describe("Wave 03: ChatEntry Like Callback", () => {
         body="Get out by 8am.  I'll count the silverware"
         timeStamp="2018-05-18T22:12:03Z"
         liked="false"
-        onClickCallback={callback}
+        updateHeartCallback={callback}
       />
-    );
+    )
 
     // Wait for it to render
-    await waitFor(() => screen.getByText("🤍"));
+    await waitFor(() => screen.getByText('🤍'))
 
-    const button = screen.getByText("🤍");
+    const button = screen.getByText('🤍')
 
-    fireEvent.click(button);
-    expect(callback).toHaveBeenCalled();
-  });
-});
+    fireEvent.click(button)
+    expect(callback).toHaveBeenCalled()
+  })
+})
