fe191f47865240bb7cde004014baad69d46f57dc
Becca Elenzil <becca@adadevelopersacademy.org>
2022-05-13 07:52:22 -0700

    update ChatEntry scaffold to remove implementation details

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,20 +1,15 @@
 import React from "react";
 import "./ChatEntry.css";
 import PropTypes from "prop-types";
-import Timestamp from "./TimeStamp";
 
 const ChatEntry = (props) => {
-  let senderClass = "chat-entry local";
-
   return (
-    <div className={senderClass}>
+    <div className="chat-entry local">
       <h2 className="entry-name">Replace with name of sender</h2>
       <section className="entry-bubble">
         <p>Replace with body of ChatEntry</p>
         <p className="entry-time">Replace with TimeStamp component</p>
-        <button onClick={props.onClickCallback} className="like">
-          🤍❤️
-        </button>
+        <button className="like">🤍</button>
       </section>
     </div>
   );
