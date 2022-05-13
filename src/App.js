adeb83ef01aa121e38da92a83cc9f6539de7cf26
Becca Elenzil <becca@adadevelopersacademy.org>
2022-05-13 08:04:29 -0700

    update imports in components to remove implementation details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "./App.css";
-import chatMessages from "./data/messages.json";
+import React from 'react';
+import './App.css';
+import chatMessages from './data/messages.json';
 
 const App = () => {
   return (
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "./ChatEntry.css";
-import PropTypes from "prop-types";
+import React from 'react';
+import './ChatEntry.css';
+import PropTypes from 'prop-types';
 
 const ChatEntry = (props) => {
   return (
