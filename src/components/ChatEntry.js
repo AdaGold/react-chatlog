6bfeb9d2db0bc8e43c2ee6cb0ac3f430fb58332e
jae <j.kerr.clayton@gmail.com>
2022-06-22 22:05:06 -0500

    fix timestamp

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
-import TimeStamp froçm './TimeStamp';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
   // FUNCTION TO CALCULATE YEARS AGO !!
