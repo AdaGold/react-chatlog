53433eabf827edac0461cfcc04f1cad22fa4556e
Tasha <tashachang2@gmail.com>
2022-12-13 16:15:48 -0800

    passed wave 01 tests for ChatEntry

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between</h1>
       </header>
       <main>
+        <ChatEntry
+        // sender={chatMessages[0].sender}
+        // body={chatMessages[0].body}
+        // timeStamp={chatMessages[0].timeStamp}
+        />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
