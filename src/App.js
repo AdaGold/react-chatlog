d0cbbf0dcc173963e88f8f6c93b730259d505b2d
Cristal <cbcodee@gmail.com>
2022-12-18 15:37:39 -0800

    wave 01

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>chats</h1>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender="cristal"
+          body="hello how are you?"
+          timeStamp="2022-12-17T22:49:33+00:00"
+        ></ChatEntry>
       </main>
     </div>
   );
