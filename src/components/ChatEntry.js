f54c9b85904808a80a245d51ab67ab54e1be6cf1
annadu <duyuan0921@gmail.com>
2023-01-26 14:12:11 -0800

    add TimeStamp wave1 done

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -7,10 +7,12 @@ import TimeStamp from './TimeStamp';
 const ChatEntry = (props) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.body}</p>
+        <p className="entry-time">
+          <TimeStamp time={props.timeStamp}/>
+        </p>
         <button className="like">🤍</button>
       </section>
     </div>
