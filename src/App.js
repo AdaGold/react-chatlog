11fb11c470b3e2beb9c754a206d27789ac808c5d
annadu <duyuan0921@gmail.com>
2023-01-26 20:45:06 -0800

    import ChatLog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+//import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog'
 
 const App = () => {
   return (
@@ -9,8 +11,10 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        
+        <ChatLog ></ChatLog>
+      
+
       </main>
     </div>
   );
