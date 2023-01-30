50c7ba3a9aee5384589d63d2a6615a97ab3a5054
annadu <duyuan0921@gmail.com>
2023-01-30 15:01:04 -0800

    import json data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-//import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog'
 
 const App = () => {
@@ -12,7 +11,7 @@ const App = () => {
       </header>
       <main>
         
-        <ChatLog ></ChatLog>
+        <ChatLog entries={chatMessages} ></ChatLog>
       
 
       </main>
