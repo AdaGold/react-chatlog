0d8da7e028cbccebd87f383dc0439331c27a74d5
Mikayla Howden <MDHowden2020@gmail.com>
2023-06-20 17:44:54 -0700

    uplifted state, added likes to the top of page, renamed application

--- src/App.js
+++ src/App.js
@@ -3,15 +3,42 @@ import './App.css';
 import chatMessages from './data/messages.json';
 import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog';
+import { useState } from 'react';
+
+
+
+//use state
+// create func to update likes
+//creat func to count likes 
+
 
 const App = () => {
+
+  const [entries, setEntries] = useState(chatMessages);
+  
+  const updateLike = (entryId) => {
+    const updatedEntries = entries.map((entry) => {
+      if (entry.id === entryId) {
+        return { ...entry, liked: !entry.liked };
+      }
+      return entry;
+    });
+
+    setEntries(updatedEntries);
+  }
+    const newLikedCount = entries.filter(
+      (entry) => entry.liked === true).length;
+
+
   return (
     <div id="App">
       <header>
         <h1> Mikayla's Application</h1>
+          <h2>{newLikedCount} ❤️s</h2>
       </header>
       <main>
-        <ChatLog entries={chatMessages} />
+        <ChatLog entries={entries}
+        updateLike={updateLike} />
       </main>
     </div>
   );
