57fc35835d6b9eb1c95ef46e3bc0c946abfeb56e
Mikelle Wade <mikelle.wade@gadadevelopersacademy.org>
2024-01-24 17:13:35 -0500

    added react-app eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,32 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, "node": true, "jest/globals": true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jest/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jest'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "no-unused-vars": "warn",
+    "no-var": "warn",
+    "no-console": "off",
+    "func-names": "off",
+    "comma-dangle": ["warn", "only-multiline"],
+    "quotes": [
+      "error",
+      "single",
+      { "allowTemplateLiterals": true, "avoidEscape": true }
+    ],
+    "camelcase": "error"
   },
 }
