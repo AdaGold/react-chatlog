307160e637a9dcbb10ded2037da521acf7c6a190
Mikelle Wade <mikelle.wade@gadadevelopersacademy.org>
2024-01-24 21:15:34 -0500

    re-ordered imports

--- src/components/ChatEntry.test.jsx
+++ src/components/ChatEntry.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
 import ChatEntry from './ChatEntry';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('Wave 01: ChatEntry', () => {
   beforeEach(() => {
