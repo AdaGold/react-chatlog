537a80ab92c9f3b980a56fcddbbc264fbb0ae43d
Mikelle Wade <mikelle.wade@gadadevelopersacademy.org>
2024-02-14 13:23:51 -0500

    configured eslint to work with vite

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,33 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, "node": true, "jest/globals": true },
+  env: { 
+    browser: true,
+    es2020: true,
+    'vitest-globals/env': true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'plugin:jest/recommended',
+    'plugin:vitest-globals/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.js'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'jest'],
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    "no-unused-vars": "warn",
-    "no-var": "warn",
-    "no-console": "off",
-    "func-names": "off",
-    "comma-dangle": ["warn", "only-multiline"],
-    "quotes": [
-      "error",
-      "single",
-      { "allowTemplateLiterals": true, "avoidEscape": true }
+    'semi': ['warn', 'always'],
+    'comma-dangle': ['warn', 'only-multiline'],
+    'quotes': [
+      'error',
+      'single',
+      { 'allowTemplateLiterals': true, 'avoidEscape': true }
     ],
-    "camelcase": "error"
+    'camelcase': 'error',
   },
 }
